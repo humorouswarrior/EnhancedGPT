5f943e0ca87692a95492247c85621e30756f76b8
Vaibhav <khandelwalv971@gmail.com>
2023-03-11 11:25:02 +0530

    cloning done

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const { response } = require("express");
 const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const configuration = new Configuration({
-    organization: "org-PMQHVNPNEoEO14dR9kClv7pn",
-    apiKey: "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",
+  organization: "org-9IDcfxolaL57Sot4zzGJjlGz",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = 5000;
 
-async function callapi() {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Say this is a test",
-      max_tokens: 7,
-      temperature: 0,
-    });
-    console.log(response.data.choices[0].text)
-}
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
 
-callapi()
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
